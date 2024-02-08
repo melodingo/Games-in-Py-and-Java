f4e8274806c6f61112f1ced396075559e808e97e
Samuel <143401480+melodingo@users.noreply.github.com>
2024-02-08 22:59:10 +0000

    Improvements

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,27 +1,32 @@
 import random
 
-def print_blackjack_table(player_hand, dealer_hand, hidden_card=True):
+def generate_random_card():
+    all_cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    suits = ["♠", "♡", "♢", "♣"]
+    all_cards = [card + suit for card in all_cards for suit in suits]
+    return random.choice(all_cards)
+
+def print_blackjack_table(player_hand, dealer_hand, reveal=False):
     print("╔═══════════════════╗")
     print("║ Dealer's Cards:   ║")
-    print("║  [ ? ]  [", end=" ")
-    if hidden_card:
-        print("? ]     ║")
+    print("║  [", dealer_hand[0], "]  [", end=" ")
+    if reveal:
+        print(dealer_hand[1], end=" ")
     else:
-        for card in dealer_hand[1:]:
-            print(card, end=" ")
-        print("]     ║")
+        print(" ? ", end=" ")
+    print("]  ║")
     print("╠═══════════════════╣")
     print("║ Your Cards:       ║")
     print("║  [", end=" ")
     for card in player_hand:
         print(card, end=" ")
-    print("]         ║")
+    print("]        ║")
     print("╠═══════════════════╣")
     print("║                   ║")
-    print("║  [1] Another card ║")
+    print("║  [1] Hit          ║")
     print("║  [2] Stand        ║")
     print("╚═══════════════════╝")
 
-player_hand = ["10", "A"]
-dealer_hand = ["7", "3"]
+player_hand = [generate_random_card(), generate_random_card()]
+dealer_hand = [generate_random_card(), generate_random_card()]
 print_blackjack_table(player_hand, dealer_hand)
