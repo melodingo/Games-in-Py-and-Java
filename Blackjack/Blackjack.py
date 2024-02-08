47afa79c997fa6f188b3b8558fc4548e079d30b0
Samuel <143401480+melodingo@users.noreply.github.com>
2024-02-08 22:42:41 +0000

    Basic Setup

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -1,1 +1,27 @@
+import random
 
+def print_blackjack_table(player_hand, dealer_hand, hidden_card=True):
+    print("╔═══════════════════╗")
+    print("║ Dealer's Cards:   ║")
+    print("║  [ ? ]  [", end=" ")
+    if hidden_card:
+        print("? ]     ║")
+    else:
+        for card in dealer_hand[1:]:
+            print(card, end=" ")
+        print("]     ║")
+    print("╠═══════════════════╣")
+    print("║ Your Cards:       ║")
+    print("║  [", end=" ")
+    for card in player_hand:
+        print(card, end=" ")
+    print("]         ║")
+    print("╠═══════════════════╣")
+    print("║                   ║")
+    print("║  [1] Another card ║")
+    print("║  [2] Stand        ║")
+    print("╚═══════════════════╝")
+
+player_hand = ["10", "A"]
+dealer_hand = ["7", "3"]
+print_blackjack_table(player_hand, dealer_hand)
