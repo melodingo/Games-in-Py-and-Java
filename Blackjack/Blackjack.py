624c5925504a1b51af605f16432137ef54621d5a
Samuel <143401480+melodingo@users.noreply.github.com>
2024-02-10 14:04:36 +0100

    Update Blackjack.py

--- Blackjack/Blackjack.py
+++ Blackjack/Blackjack.py
@@ -9,13 +9,13 @@ def generate_random_card():
 
 def print_blackjack_table(player_hand, dealer_hand, reveal=False):
     max_player_card_length = max(len(card) for card in player_hand)
-    max_card_length = max(max_player_card_length, 3)  
+    max_card_length = max(max_player_card_length, 3)
     print("╔═══════════════════╗")
     print("║ Dealer's Cards:   ║")
     print("║  [ {:<{}} ]  [ {:<{}} ] ║".format(dealer_hand[0], max_card_length, dealer_hand[1] if reveal else "?", max_card_length))
     print("╠═══════════════════╣")
     print("║ Your Cards:       ║")
-    print("║  [ {:<{}} ]    ║".format("]  [ ".join(player_hand), max_card_length))
+    print("║  [ {:<{}} ]   ║".format("]  [ ".join(player_hand), max_card_length))
     print("╠═══════════════════╣")
     print("║                   ║")
     print("║  [1] Hit          ║")
@@ -26,14 +26,17 @@ player_hand = [generate_random_card(), generate_random_card()]
 dealer_hand = [generate_random_card(), generate_random_card()]
 print_blackjack_table(player_hand, dealer_hand)
 
+draw_count = 0
+
 while True:
-    if keyboard.is_pressed("space"):
-        
+    if keyboard.is_pressed("enter"):
+        print("\nRevealing dealer's hidden card:")
         print_blackjack_table(player_hand, dealer_hand, reveal=True)
     
-    if keyboard.is_pressed("enter"):
-        print("enter")
+    if keyboard.is_pressed_released("space") and draw_count < 2:
+        player_hand.append(generate_random_card())
+        draw_count += 1
+        print_blackjack_table(player_hand, dealer_hand)
 
     if keyboard.is_pressed("esc"):
         break
-
